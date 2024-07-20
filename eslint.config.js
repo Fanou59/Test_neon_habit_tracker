17592ba977d8759ddae0de2fff63a4875a0185e9
Ste[PH]ane <steph.guery@gmail.com>
2024-07-20 22:58:16 +0200

    fix: :bug: Modif du fichier de confi eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,23 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
+export default {
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+      ...globals.node, // Ajoute les variables globales de Node.js
+    },
+    ecmaVersion: 2021, // Utilise les fonctionnalités ECMAScript 2021
+    sourceType: "module", // Utilise les modules ES
+  },
+  plugins: {
+    "eslint-plugin-node": require("eslint-plugin-node"), // Assure que le plugin node est installé
+  },
+  extends: [
+    pluginJs.configs.recommended,
+    "plugin:node/recommended", // Utilise les règles recommandées pour Node.js
+  ],
+  rules: {
+    // Ajoutez des règles spécifiques ici si nécessaire
+  },
+};
